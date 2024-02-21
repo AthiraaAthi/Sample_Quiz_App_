ecedc741cef9701f5ad7582bbb1fff18b105790d
AthiraaAthi <athiraa2612@gmail.com>
2024-02-21 14:08:53 +0530

    added result screen

--- lib/view/answer_card.dart
+++ lib/view/answer_card.dart
@@ -21,33 +21,80 @@ class AnswerCard extends StatelessWidget {
     bool isCorrectAnswer = currentIndex == correctAnswerIndex;
     bool isWrongAnswer = !isCorrectAnswer && isSelected;
     return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 10.0,
-      ),
-      child: Container(
-        height: 70,
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: Colors.white10,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: isCorrectAnswer
-                ? Colors.green
-                : isWrongAnswer
-                    ? Colors.red
-                    : Colors.white24,
-          ),
+        padding: EdgeInsets.symmetric(
+          vertical: 10.0,
         ),
-        child: Row(
-          children: [
-            Expanded(
-                child: Text(
-              question /*mentioned above,not the modelcls*/,
-              style: TextStyle(fontSize: 16),
-            ))
-          ],
-        ),
-      ),
-    );
+        child: selectedAnswerIndex != null
+            //condition
+            ? Container(
+                height: 70,
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: isCorrectAnswer
+                        ? Colors.green
+                        : isWrongAnswer
+                            ? Colors.red
+                            : Colors.white24,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        question /*mentioned above,not the modelcls*/,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    isCorrectAnswer
+                        ? buildCorrectIcon()
+                        : isWrongAnswer
+                            ? buildWrongIcon()
+                            : SizedBox.shrink()
+                  ],
+                ),
+              )
+            : Container(
+                height: 70,
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.white24,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      question /*mentioned above,not the modelcls*/,
+                      style: TextStyle(fontSize: 16),
+                    ))
+                  ],
+                ),
+              ));
   }
 }
+
+Widget buildCorrectIcon() => CircleAvatar(
+      radius: 15,
+      backgroundColor: Colors.green,
+      child: Icon(
+        Icons.check,
+        color: Colors.white,
+      ),
+    );
+Widget buildWrongIcon() => CircleAvatar(
+      radius: 15,
+      backgroundColor: Colors.red,
+      child: Icon(
+        Icons.close,
+        color: Colors.white,
+      ),
+    );
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,13 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:sample_quiz_app/view/answer_card.dart';
 import 'package:sample_quiz_app/view/questions.dart';
 import 'package:sample_quiz_app/view/questionss.dart';
+import 'package:sample_quiz_app/view/result_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int? selectedAnswerIndex;
+  int questionIndex = 0;
+  int score = 0;
+
+  void pickAnswer(int value) {
+    selectedAnswerIndex = value;
+    final question = questions[questionIndex];
+    if (selectedAnswerIndex == question.correctAnswerIndex) {
+      score++;
+    }
+    setState(() {});
+  }
+
+  void goToNextQuestion() {
+    if (questionIndex < questions.length - 1) {
+      questionIndex++;
+      selectedAnswerIndex = null;
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final QuestionModel = questions[0];
+    final QuestionModel = questions[questionIndex];
+    bool isLastQuestion = questionIndex == questions.length - 1;
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Quiz"),
@@ -28,17 +56,87 @@ class HomeScreen extends StatelessWidget {
               itemCount: QuestionModel.options.length,
               itemBuilder: (context, index) {
                 return GestureDetector(
-                  onTap: () {},
+                  onTap: selectedAnswerIndex == null
+                      ? () => pickAnswer(index)
+                      : null,
                   child: AnswerCard(
                     currentIndex: index,
                     question: QuestionModel.options[index],
-                    isSelected: false,
-                    selectedAnswerIndex: 0,
+                    isSelected: selectedAnswerIndex == index,
+                    selectedAnswerIndex: selectedAnswerIndex,
                     correctAnswerIndex: QuestionModel.correctAnswerIndex,
                   ),
                 );
               },
-            )
+            ),
+            isLastQuestion
+                ? InkWell(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ResultScreen(
+                              score: score,
+                            ),
+                          ));
+                    },
+                    child: Container(
+                      color: Colors.white10,
+                      height: 50,
+                      width: 320,
+                      child: Center(
+                          child: Text(
+                        "Finish",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                    ),
+                  )
+                // ElevatedButton(
+                //     style: ButtonStyle(
+                //         shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                //             borderRadius: BorderRadius.circular(20))),
+                //         backgroundColor:
+                //             MaterialStatePropertyAll(Colors.white10)),
+                //     onPressed: () {
+                //       Navigator.pushReplacement(
+                //           context,
+                //           MaterialPageRoute(
+                //             builder: (context) => ResultScreen(),
+                //           ));
+                //     },
+                //     child: Text(
+                //       "Finish",
+                //       style: TextStyle(color: Colors.white),
+                //     ))
+                : InkWell(
+                    onTap:
+                        selectedAnswerIndex != null ? goToNextQuestion : null,
+                    child: Container(
+                      color: Colors.white10,
+                      height: 50,
+                      width: 320,
+                      child: Center(
+                          child: Text(
+                        "Next",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                    ),
+                  )
+            // ElevatedButton(
+            //     style: ButtonStyle(
+            //         shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+            //           RoundedRectangleBorder(
+            //               borderRadius: BorderRadius.circular(.5),
+            //               side: BorderSide(color: Colors.white10)),
+            //         ),
+            //         backgroundColor:
+            //             MaterialStatePropertyAll(Colors.white10)),
+            //     onPressed:
+            //         selectedAnswerIndex != null ? goToNextQuestion : null,
+            //     child: Text(
+            //       "Next",
+            //       style: TextStyle(color: Colors.white),
+            //     ))
           ],
         ),
       ),
